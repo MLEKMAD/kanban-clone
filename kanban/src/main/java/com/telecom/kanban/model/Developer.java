6ea0b322bf196e1b8a80674eba1da67317bd03f7
LEKMAD <mohamed.lekamd@um5s.net.ma>
2021-10-20 12:29:04 +0200

    Fixed models

--- kanban/src/main/java/com/telecom/kanban/model/Developer.java
+++ kanban/src/main/java/com/telecom/kanban/model/Developer.java
@@ -1,12 +1,14 @@
 package com.telecom.kanban.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
@@ -34,6 +36,9 @@ public class Developer {
 	@Column(unique = true)
 	@Email(message = "Email")
 	private String email;
+	
+	@ManyToMany
+	private List<Task> tasks;
 	
 	
 }
--- kanban/src/main/java/com/telecom/kanban/model/Task.java
+++ kanban/src/main/java/com/telecom/kanban/model/Task.java
@@ -1,11 +1,13 @@
 package com.telecom.kanban.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
@@ -23,4 +25,8 @@ public class Task {
 	private TaskType taskType;
 	private TaskStatus taskStatus;
 	
+	@ManyToMany(mappedBy = "tasks")
+	private List<Developer> developers;
+	
+
 }

f6412a03eea6084116e6f4d4c169995335d4c2d6
LEKMAD <mohamed.lekamd@um5s.net.ma>
2021-10-26 18:47:30 +0200

    Added services implementation

--- kanban/src/main/java/com/telecom/kanban/service/impl/TaskServiceImpl.java
+++ kanban/src/main/java/com/telecom/kanban/service/impl/TaskServiceImpl.java
@@ -1,6 +1,7 @@
 package com.telecom.kanban.service.impl;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import com.telecom.kanban.model.Task;
@@ -13,6 +14,7 @@ public class TaskServiceImpl implements TaskService {
 
 	private TaskRepo taskRepo;
 	private TaskStatusRepo taskstatusRepo;
+
 	@Override
 	public Collection<Task> findAllTasks() {
 		return taskRepo.findAll();
@@ -25,14 +27,28 @@ public class TaskServiceImpl implements TaskService {
 
 	@Override
 	public Task moveRightTask(Task task) {
+		List<TaskStatus> allStatus = taskstatusRepo.findAll();
 		TaskStatus taskStatus = task.getTaskStatus();
-		return null;
+		for (TaskStatus status : allStatus) {
+			if(taskStatus.getId() == status.getId()) {
+				int index = allStatus.indexOf(status);
+				if(index != allStatus.size()) task.setTaskStatus(allStatus.get(index+1));
+			}
+		}
+		return task;
 	}
 
 	@Override
 	public Task moveLeftTask(Task task) {
+		List<TaskStatus> allStatus = taskstatusRepo.findAll();
 		TaskStatus taskStatus = task.getTaskStatus();
-		return null;
+		for (TaskStatus status : allStatus) {
+			if(taskStatus.getId() == status.getId()) {
+				int index = allStatus.indexOf(status);
+				if(index != 0) task.setTaskStatus(allStatus.get(index-1));
+			}
+		}
+		return task;
 	}
 
 }

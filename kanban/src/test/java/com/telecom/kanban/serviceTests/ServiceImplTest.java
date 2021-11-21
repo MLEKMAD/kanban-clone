bbc0a75a99a04c903ad1ba942dbe0ffc58c44421
LEKMAD <mohamed.lekamd@um5s.net.ma>
2021-11-22 00:49:35 +0100

    Added test service file

---
+++ kanban/src/test/java/com/telecom/kanban/serviceTests/ServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.telecom.kanban.serviceTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServiceImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
